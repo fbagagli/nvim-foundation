0bcf9a8f51b743b6bcaae41b650c4354ff7626f6
Bagagli Francesco 00537896 <f.bagagli@huawei.com>
2026-02-17 13:32:42 +0100

    add stylua and removed formatting C/C++

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,20 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	opts = {
-		notify_on_error = false,
-		format_on_save = {
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-		formatters_by_ft = {
-			python = { "ruff_format", "ruff_fix" }, -- formatting and import sorting
-			["*"] = { "trim_whitespace" },
-		},
-	},
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    -- notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Define filetypes where LSP fallback formatting should be disabled
+      local disable_filetypes = { c = true, cpp = true }
+      return {
+        timeout_ms = 500,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
+    formatters_by_ft = {
+      python = { "ruff_format", "ruff_fix" }, -- formatting and import sorting
+      ["*"] = { "trim_whitespace" },
+    },
+  },
 }
