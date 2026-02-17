73aea7dcdd1533cfcc6af0f0fb5c3deb3668ecef
Bagagli Francesco 00537896 <f.bagagli@huawei.com>
2026-02-17 12:51:50 +0100

    add trim whitespace and lua formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,15 +1,16 @@
 return {
-  'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
-  opts = {
-    notify_on_error = false,
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    formatters_by_ft = {
-      python = { 'ruff_format', 'ruff_fix' }, -- formatting and import sorting
-    },
-  },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		notify_on_error = false,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		formatters_by_ft = {
+			python = { "ruff_format", "ruff_fix" }, -- formatting and import sorting
+			["*"] = { "trim_whitespace" },
+		},
+	},
 }
