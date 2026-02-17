339e2acd8dc511eeaa9fdb6865c04353bb5048f3
Bagagli Francesco 00537896 <f.bagagli@huawei.com>
2026-02-17 12:35:53 +0100

    add debug adapter for C

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,71 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		-- Creates a beautiful debugger UI
+		"rcarriga/nvim-dap-ui",
+		-- Required dependency for nvim-dap-ui
+		"nvim-neotest/nvim-nio",
+		-- Installs the debug adapters automatically via Mason
+		"williamboman/mason.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+
+		-- Configure Mason to install the cppdbg adapter
+		require("mason-nvim-dap").setup({
+			automatic_installation = true,
+			handlers = {
+				function(config)
+					-- Instructs mason-nvim-dap to automatically configure and register the adapter
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			},
+			-- cppdbg interfaces with gdb out of the box
+			ensure_installed = { "cppdbg" },
+		})
+
+		-- Setup DAP UI
+		dapui.setup()
+
+		-- Automatically open and close the UI when debugging starts/ends
+		dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+		dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+		dap.listeners.before.event_exited["dapui_config"] = dapui.close
+
+		-- C Configuration
+		dap.configurations.c = {
+			{
+				name = "Launch executable",
+				type = "cppdbg",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopAtEntry = true,
+				setupCommands = {
+					{
+						text = "-enable-pretty-printing",
+						description = "enable pretty printing",
+						ignoreFailures = false,
+					},
+				},
+			},
+		}
+
+		-- Map C++ to the exact same configuration as C
+		dap.configurations.cpp = dap.configurations.c
+
+		-- Keymaps for debugging control
+		vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
+		vim.keymap.set("n", "<F10>", dap.step_over, { desc = "Debug: Step Over" })
+		vim.keymap.set("n", "<F11>", dap.step_into, { desc = "Debug: Step Into" })
+		vim.keymap.set("n", "<F12>", dap.step_out, { desc = "Debug: Step Out" })
+		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+		vim.keymap.set("n", "<leader>B", function()
+			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+		end, { desc = "Debug: Set Conditional Breakpoint" })
+	end,
+}
